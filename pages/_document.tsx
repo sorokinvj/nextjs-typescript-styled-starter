7c99dfa2a966d53745c05cb442ff28230916c4ee
Vladislav Sorokin <sorokinvj@gmail.com>
2020-12-18 22:12:26 +0000

    fix document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,26 @@
-import Document from 'next/document'
+import React from 'react'
+import Document, { Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document<any> {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
 
     try {
+      // wraps the collectStyles provider around our <App />.
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) =>
+          enhanceApp: (App) => (props): React.ReactElement =>
             sheet.collectStyles(<App {...props} />),
         })
 
+      // extract the initial props that may be present.
       const initialProps = await Document.getInitialProps(ctx)
+
+      // returning the original props together with our styled components.
       return {
         ...initialProps,
         styles: (
@@ -26,5 +33,17 @@ export default class MyDocument extends Document<any> {
     } finally {
       sheet.seal()
     }
+  }
+
+  render(): JSX.Element {
+    return (
+      <html lang="en">
+        <Head>{this.props.styleTags}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
   }
 }
