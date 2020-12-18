5d860158081aefdd74a31d197bd1e54848c2d532
Vladislav Sorokin <sorokinvj@gmail.com>
2020-12-18 22:02:39 +0000

    changed document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import Document from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document<any> {
-  static async getInitialProps(ctx: NextDocumentContext) {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
 
